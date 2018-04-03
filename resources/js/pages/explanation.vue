8efd6d298e78fc1bb6cf4b864082892aaf645910
dionpisas <dion.zadkine@gmail.com>
2018-04-04 01:25:10 +0200

    Jumbo class aangemaakt met titel en logo, gestyled voor desktop en mobile| uitlegpagina Kleur styling van headers aangepast| uitlegpagina Grid gemaakt zodat het layout goed werkt voor desktop en mobile| uitlegpagina Afbeeldingen toegevoegd en aangepast| uitlegpagina Teksten en layout aangepast plekken voor afbeeldingen

--- resources/js/pages/explanation.vue
+++ resources/js/pages/explanation.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!--jumbo-->
-        <div id="jumbo" class="md-layout-item md-layout md-gutter md-elevation-5">
+        <div id="jumbo" class="md-layout-item md-layout md-gutter md-elevation-8">
             <div class="md-layout-item">
                 <p id="head">Virgin Says</p>
                 <img id="logo" src="images/test.jpg" alt="">
@@ -9,17 +9,115 @@
         </div>
         <!--End jumbo-->
 
+        <div class="parallax">
         <!--content-->
-        <div class="md-layout-item md-layout md-gutter">
-            <div class="md-layout-item">
-                <p id="qr-text">Scan de QR-code.</p>
-                <img id="qr" src="images/2.png" alt="scan de qr-code">
+        <div id="box_1" class="md-layout-item md-layout md-gutter md-elevation-7">
+            <div id="box_1_inside" class="md-layout-item">
+                <p class="header">Scan de QR-code.</p>
+                <!--<div class="md-small-hide">-->
+                    <!--<img id="qr" src="images/2.png" alt="scan de qr-code" >-->
+                <!--</div>-->
+                <p class="text"> Door de qr-code te scannen krijgt u de mogelijkheid om het spel op te starten ook
+                    is er een link die naar deze uitleg pagina komt. </p>
             </div>
-            <div class="md-layout-item">
+        </div>
 
+        <div id="second" class="md-layout-item md-layout md-gutter md-elevation-6">
+            <div class="md-layout-item">
+                <p class="header">Doel</p>
+
+                <img src="" alt="">
+
+                <p class="text">
+                    Voer de opdrachten in de goeie volgorde uit en krijg de hoogste score van de maand.
+                </p>
+            </div>
+        </div>
+
+        <div id="third" class="md-layout-item md-layout md-gutter md-elevation-5">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">Voordat je begin</p>
+
+                <!--image-->
+                <img src="" alt="">
+
+                <!--text-->
+                <p class="text">
+                    Als het spel gaat beginnen krijgt u de mogelijkheid om aan te geven met hoeveel personen u gaat spelen.
+                </p>
+            </div>
+        </div>
+
+        <div id="fourth" class="md-layout-item md-layout md-gutter md-elevation-4">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">Spelers</p>
+                <!--image-->
+                <img src="" alt="">
+                <!--text-->
+                <p class="text">
+                    Bij dit spel kunt u met max 5 personen spelen, waarvan 1 de opdrachtgever zal zijn en de rest uitvoert.
+                    Bij 4 spelers of minder worden de opdrachten automatisch gegenereerd.
+                </p>
+            </div>
+        </div>
+
+        <div id="fifth" class="md-layout-item md-layout md-gutter md-elevation-2">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">Het spel spelen</p>
+                <!--image-->
+                <img src="" alt="">
+                <!--text-->
+                <p class="text">
+                    Om elke hoek is er een touchpad met licht ingebouwd. Via uw telefoon krijgt u de opdracht om op een touchpad te gaan staan, die dezelfde kleur heeft als aangegeven staat in de opdracht.
+                    Hiervoor zal u een beperkte tijd hebben.
+                </p>
+            </div>
+        </div>
+
+        <div id="sixth" class="md-layout-item md-layout md-gutter md-elevation-3">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">3 kansen anders game over</p>
+                <!--image-->
+                <img src="" alt="">
+                <!--text-->
+                <p class="text">
+                    Lukt het je niet om in minder dan 3 kansen de opdracht af te maken dan is het game over.
+                </p>
+            </div>
+        </div>
+
+        <div id="seventh" class="md-layout-item md-layout md-gutter md-elevation-1">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">Moeilijkheidsgraad</p>
+                <!--image-->
+                <img src="" alt="">
+                <!--text-->
+                <p class="text">
+                    Tijdens het spelen zal de moeilijkheidsgraad steeds hoger worden dus met andere woorden: kortere tijden om te reageren en de opdracht uit te voeren.
+                </p>
+            </div>
+        </div>
+
+        <div id="eight" class="md-layout-item md-layout md-gutter">
+            <div class="md-layout-item">
+                <!--header-->
+                <p class="header">Topspelers</p>
+                <!--image-->
+                <img src="" alt="">
+                <!--text-->
+                <p class="text">
+                    Heeft u met uw team een hele hoge score behaald? Dan zal de groepsnaam in de top 10 van de maand komen. Lekker om mee te patsen natuurlijk!
+                </p>
             </div>
         </div>
         <!--end content-->
+        </div>
+
     </div>
 </template>
 
